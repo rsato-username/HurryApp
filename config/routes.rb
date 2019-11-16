4cf246d17ad32779d47cfd463c904c9838c70fed
rsato-username <satoheisuper@gmail.com>
2019-11-16 13:50:04 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'fronts#index'
-  resources :users, only: [:index, :edit, :update]
-  resources :orders, only: :index
+  resources :users, only: [:edit, :update]
+  resources :orders, only: [:index, :new, :create]
+  # resources :drinks, only: [:index, :new]
   # resources :fronts, only: :index
+  # namespace :admin do
+  #   resources :foods
+  #   resources :drinks 
+  #   resources :users 
+  #   resources :orders 
+  #   resources :reviews 
+  #   resources :salespersons
+  # end
 end

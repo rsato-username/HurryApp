b7c5699ba1fd5cf4cbfd6f90e82f341564bd7474
rsato-username <satoheisuper@gmail.com>
2019-11-16 13:48:34 +0900

    up controller

---
+++ app/controllers/admin/foods_controller.rb
@@ -0,0 +1,7 @@
+class Admin::FoodsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
---
+++ test/controllers/admin/salespersons_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::SalespersonsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

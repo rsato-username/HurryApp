66cf3c2b0849d01d04ee697939f1d1ed8d7805e6
rsato-username <satoheisuper@gmail.com>
2019-12-15 13:01:55 +0900

    Update review.js

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,8 +1,8 @@
-$(function() {
-  $('#star').raty({
-    size     : 36,
-    starOff:  asset_path('star-off.png'),
-    starOn : asset_path('star-on.png'),
-    scoreName: 'review[rate]',
-  });
-});
+// $(function() {
+//   $('#star').raty({
+//     size     : 36,
+//     starOff:  asset_path('star-off.png'),
+//     starOn : asset_path('star-on.png'),
+//     scoreName: 'review[rate]',
+//   });
+// });

b43c0b97592f2a567f024eed4deb8739cdd7d32d
rsato-username <satoheisuper@gmail.com>
2019-12-18 15:17:56 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'fronts#index'
+  resources :fronts, only: :index do
+    collection do
+      get :home
+    end
+  end
   resources :users, only: [:edit, :update]
   resources :orders, only: [:index, :new, :create]
   resources :reviews, only: [:index, :new, :create]

cb88de071579542933896bf0547508b275fb1556
rsato-username <satoheisuper@gmail.com>
2019-12-22 14:52:35 +0900

    add image and delete, delete model

--- db/migrate/20191112123741_create_order_foods.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrderFoods < ActiveRecord::Migration[5.2]
-  def change
-    create_table :order_foods do |t|
-      t.references :order,      foreign_key: true
-      t.references :food,       foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20191112124406_create_order_salespeople.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrderSalespeople < ActiveRecord::Migration[5.2]
-  def change
-    create_table :order_salespeople do |t|
-      t.references :order,             foreign_key: true
-      t.references :salesperson,       foreign_key: true
-      t.timestamps
-    end
-  end
-end
